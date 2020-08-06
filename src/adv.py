1a5a39bc0f815fbd3d9c333fa530ecf35167bdc2
Poenomaly <djbenson741@gmail.com>
2020-08-06 13:21:13 -0600

    Completed MVP for day 2. Items are storable and useable. Next step is to give these useable items a purpose.

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,16 @@
 from room import Room
 from player import Player
+from items import Item
+
+# Declare the items
+
+items = {
+    'torch': Item('Torch',
+                  "A small torch used for lighting dark passages."),
+
+    'key': Item("Treasure Key",
+                "A glimmering gold key used to unlock the treasure room.")
+}
 
 # Declare all the rooms
 
@@ -8,14 +19,17 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",
+                items = [items['torch']]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",
+                items = [items['key']]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",
+                items_useable=[items['key'].name, items['torch'].name]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -45,7 +59,7 @@ player = Player("test_dummy", room['outside'])
 def input_parser():
 
     print("enter one of the following keys: ")
-    command = input("'n' (go north), 's' (go south), 'e' (go east), 'w' (go west), or 'q' (quit game)\n")
+    command = input("'n' (go north), 's' (go south), 'e' (go east), 'w' (go west), 'p' (pickup item), 'u' (use item), or 'q' (quit game)\n")
     # Convert to lower case in the chance that one of the keys is entered as a capital
     return command.lower() 
 
@@ -65,7 +79,6 @@ print(f"Welcome {player.name} to Adventure Text.")
 
 # Creates infinite loop that only exits when players quits the game
 while True:
-    print(player.__str__()) # print out player's movement into the current room
     print(player.current_room.__str__())
     # Upon user input, create a loop that checks the user's input
     # Start by checking if input is recognized as an acceptable response - if not, tell the player to try again
@@ -84,7 +97,7 @@ while True:
 
         if player.current_room.n_to:
                 player.current_room = player.current_room.n_to # move player to the room to the north
-        
+                print(player.__str__()) # print out player's movement into the new current room
         else:
         # If n_to room does not exist in current room, tell the player and have them input a new command.
             print("You have run into a dead end. Try selecting a different direction.")
@@ -94,7 +107,7 @@ while True:
 
         if player.current_room.s_to is not None:
             player.current_room = player.current_room.s_to # move player to the room to the south
-        
+            print(player.__str__()) # print out player's movement into the new current room
         else:
             # If s_to room does not exist in current room, tell the player and have them input a new command.
             print("You have run into a dead end. Try selecting a different direction.")
@@ -104,7 +117,8 @@ while True:
 
         if player.current_room.e_to is not None:
             player.current_room = player.current_room.e_to # move player to the room to the east
-        
+            print(player.__str__()) # print out player's movement into the new current room
+
         else:
             # If e_to room does not exist in current room, tell the player and have them input a new command.
             print("You have run into a dead end. Try selecting a different direction.")
@@ -118,7 +132,31 @@ while True:
         else:
             # If w_to room does not exist in current room, tell the player and have them input a new command.
             print("You have run into a dead end. Try selecting a different direction.")
+            print(player.__str__()) # print out player's movement into the new current room
 
+    if command == 'p':
+        # Check if there are items in the current room
+        if player.current_room.items:
+            player.add_to_inventory(player.current_room.items[0]) # add the item to player inventory
+            player.current_room.remove_item_from_room(player.current_room.items[0]) # remove the item from the room
+        else:
+            print("There are no items available in this room.") # inform player there are no available items to pick up
+            continue
+
+    if command == 'u':
+        # check if there are any items for player to use
+        # print(f"In this room you can use {player.current_room.items_useable}") Testing purposes
+        if player.inventory:
+            print("Please select which item you would like to use: ")
+            selected_item = input([f"{i.name}, {i.description}" for i in player.inventory])
+            # print(selected_item) testing purposes
+            if selected_item.capitalize() in player.current_room.items_useable:
+                print("You can use that here")
+            else:
+                print("You have no items you can use in this room.")
+
+        else:
+            print("You have no items you can use at this time.")
 
 # if __name__ == "__main__":
 #     # Test that our player class and room class are working correctly
--- src/player.py
+++ src/player.py
@@ -4,10 +4,19 @@
 
 class Player:
     # set a default for name and current_room? player should always start outside
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory=[]):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
     
-    # TODO: create __str__ method to print out player movement.
+    # print out player movement.
     def __str__(self):
         return f"You have entered the {self.current_room.name}."
+
+    def add_to_inventory(self, item):
+        # add item to inventory
+        self.inventory.append(item)
+        self.print_inventory()
+
+    def print_inventory(self):
+        print("Your inventory contains: ", [f"{item.name}, {item.description}" for item in self.inventory],'\n')
